2e0ba04c3c24af7faceb126993b501cdd79ffbdc
facuabarcadev <facuabarca.dev@gmail.com>
2025-02-15 19:42:29 +0100

    chore: initialize project with Node.js, Express, TypeScript

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes";
+
+// Cargar variables de entorno
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+// Rutas
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
